20f484ab2a94d87d51deb9479c65c21ff230c2c4
parthasarathydNU <parthasarathy.d@northeastern.edu>
2023-01-30 21:16:05 -0500

    Added clear board button

--- src/Components/PathFinderViz/PathFinderViz.tsx
+++ src/Components/PathFinderViz/PathFinderViz.tsx
@@ -7,17 +7,50 @@ import {dijkstra}  from "../../Algos/Dijkstra"
 function PathFinderViz() {
   
     const [grid, setStateGrid] = useState<GridRow[]>([]);
+    const [isVizRunning, setIsVizRunning] = useState<boolean>(false);
+    const [isBoardClear, setIsBoardClear] = useState<boolean>(true);
 
     const START_NODE_ROW = 5;
     const START_NODE_COL = 5;
     const FINISH_NODE_ROW = 10;
     const FINISH_NODE_COL = 30;
 
+    const generateBoard = () : void => {
+        const temp:GridRow[] = [];
+        // create 15 rows and 50 columns
+        for(let i = 0; i < 15 ; i++){
+            let row:GridCell[] = [];
+            for (let j = 0; j < 50; j++) {
+                
+                row.push(generateCell(i, j));
+            }
+            temp.push(row);
+
+        }
+
+        setStateGrid(temp);
+        setIsBoardClear(true);
+    }
+
+    const clearBoard = () : void => {
+
+        if(!isVizRunning && !isBoardClear){
+            generateBoard();
+        }
+
+        
+    }
+
 
     const updateViz = (visitedNodes : Array<GridCell>) => {
+
+        setIsVizRunning(true);
+        setIsBoardClear(false);
         
         // here we update the state of the grid
         // we iterate through the visited nodes in order and re render the grid
+
+        let count = 0;
         
         // let's iterate through the visited nodes and update them in the state
         for( let i  = 0 ; i < visitedNodes.length; i++){
@@ -37,20 +70,41 @@ function PathFinderViz() {
 
                 setStateGrid(newGrid);
 
+                count++;
+
+                if(count == visitedNodes.length - 1){
+                    setIsVizRunning(false);
+                }
+
             }, 5 * i);
+            
+
+            // setTimerId(newTimer);
         }
+
+
+        
     }
 
     const runDijkstra = () => { 
-        console.log('run dijkstra');
+
+        if(!isVizRunning && isBoardClear){
+
+            console.log('run dijkstra');
+
+            setIsVizRunning(true);
+    
+    
+            // For the dijkstra's algorithm, we need to pass in the
+            // grid, the start node and the end node
+            let visitedNodes = dijkstra(grid, grid[START_NODE_ROW][START_NODE_COL], grid[FINISH_NODE_ROW][FINISH_NODE_COL] );
+    
+            // console.log(visitedNodes);
+            updateViz(visitedNodes);
+
+        }
 
 
-        // For the dijkstra's algorithm, we need to pass in the
-        // grid, the start node and the end node
-        let visitedNodes = dijkstra(grid, grid[START_NODE_ROW][START_NODE_COL], grid[FINISH_NODE_ROW][FINISH_NODE_COL] );
-
-        // console.log(visitedNodes);
-        updateViz(visitedNodes);
 
     }
 
@@ -58,19 +112,9 @@ function PathFinderViz() {
     
     useEffect(() => {
       
-        const temp:GridRow[] = [];
-        // create 15 rows and 50 columns
-        for(let i = 0; i < 15 ; i++){
-            let row:GridCell[] = [];
-            for (let j = 0; j < 50; j++) {
-                
-                row.push(generateCell(i, j));
-            }
-            temp.push(row);
 
-        }
+        generateBoard();
 
-        setStateGrid(temp);
     
       
     }, [])
@@ -95,18 +139,20 @@ function PathFinderViz() {
     }
     
   return (
-    <>
-    <button onClick={runDijkstra}>Run Dijkstra</button>
-    <div className="grid">
+    <section className='wrapper'>
+        <button onClick={runDijkstra} >Run Dijkstra</button>
+        <button onClick={clearBoard} >Clear Board</button>
 
-        {grid?.length > 0 && grid.map( (row, idx) => (
-            <div className='row' key={idx}>
-                {row.map( (node,id) => (<GridCellNode key={id} isStart={node.isStart} isFinish={node.isFinish} isVisited={node.isVisited} />))}
-            </div>
-            
-        ))}
-    </div>
-    </>
+        <div className="grid">
+
+            {grid?.length > 0 && grid.map( (row, idx) => (
+                <div className='row' key={idx}>
+                    {row.map( (node,id) => (<GridCellNode key={id} isStart={node.isStart} isFinish={node.isFinish} isVisited={node.isVisited} />))}
+                </div>
+                
+            ))}
+        </div>
+    </section>
   )
 }
 
